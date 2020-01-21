704b8fc8696b66f6b330c473c5ad7e58d566a9aa
patryk-kilian <patryk.kilian93@gmail.com>
2020-01-21 21:34:01 +0100

    Updates

---
+++ precache-manifest.da169040eba0f710d5befd58918a599f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb58dc57bcecb100fa86350610a1352",
+    "url": "/movie-box/index.html"
+  },
+  {
+    "revision": "5524699a358e3579a2b8",
+    "url": "/movie-box/static/css/main.adb314cd.chunk.css"
+  },
+  {
+    "revision": "5ee6f5c675a72c776169",
+    "url": "/movie-box/static/js/2.305f2e27.chunk.js"
+  },
+  {
+    "revision": "5524699a358e3579a2b8",
+    "url": "/movie-box/static/js/main.fac7b8fd.chunk.js"
+  },
+  {
+    "revision": "65b90c459c82e33c0ded",
+    "url": "/movie-box/static/js/runtime-main.729d1f89.js"
+  }
+]);
